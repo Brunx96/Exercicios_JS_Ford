bcf3e8e5fbd059542d9bb97bbbae047f15819f88
Ruan Macionil <macionilruan@gmail.com>
2024-03-19 16:35:45 -0300

    TreinoJavaScript

--- Exercicios/jogoFutebol.js
+++ Exercicios/jogoFutebol.js
@@ -12,3 +12,19 @@
 
 //saida
 // = informar o resultado do jogo de futebol
+
+let golsTime1 = parseInt(prompt("Digite a quantidade de gols do Time 1:"));
+let golsTime2 = parseInt(prompt("Digite a quantidade de gols do Time 2:"));
+
+function determinarResultado(golsTime1, golsTime2) {
+    if (golsTime1 === golsTime2) {
+        alert ("Empate");
+    } else if (golsTime1 > golsTime2) {
+        alert ("Vitória do Time 1");
+    } else {
+        alert ("Vitória do Time 2");
+    }
+}
+
+let resultado = determinarResultado(golsTime1, golsTime2);
+console.log(`O resultado do jogo foi: ${resultado}`);
